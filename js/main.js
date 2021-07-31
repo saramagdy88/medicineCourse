6d946b87972e63c98554fac68e05d39ded92d2a4
SaraMagdy <sm9976928@gmail.com>
2021-07-31 22:27:27 +0200

    upload medicine courses

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Select element
+let btn = document.querySelector('#send');
+let result= document.querySelector('#result');
+btn.addEventListener('click' ,function(e){
+
+   let newText="<h3>Thank You For Contacting Us</h3>";
+   result.innerHTML=newText;
+ 
+
+
+  
+
+})
